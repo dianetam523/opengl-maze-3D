784892d42bb4e3fc230657a74a40ad0098f7a6b5
dianetam523 <diane.tam523@gmail.com>
2016-03-30 19:44:52 -0400

    Introduced more uniform variables to allow us to use the same data in a VAO and apply transformations via matrices

--- src/shaders/StaticShader.java
+++ src/shaders/StaticShader.java
@@ -1,11 +1,14 @@
 package shaders;
 
+import org.lwjgl.util.vector.Matrix4f;
+
 public class StaticShader extends ShaderProgram{
 	
 	public static final String VERTEX_FILE = "src/shaders/vertexShader.txt";
 	public static final String FRAGMENT_FILE = "src/shaders/fragmentShader.txt";
-
 	
+	private int location_transformationMatrix;
+
 	public StaticShader() {
 		super(VERTEX_FILE, FRAGMENT_FILE);
 	}
@@ -15,6 +18,15 @@ public class StaticShader extends ShaderProgram{
 	protected void bindAttributes() {
 		super.bindAttribute(0, "postion");
 		super.bindAttribute(1, "textureCoords");
+	}
+
+	@Override
+	protected void getAllUniformLocations() {
+		location_transformationMatrix = super.getUniformLocation("transformationMatrix");
+	}
+	
+	public void loadTransformatioNMatrix(Matrix4f matrix){
+		super.loadMatrix(location_transformationMatrix, matrix);
 	}
 
 }
