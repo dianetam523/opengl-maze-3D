dfe1f5e0a7c87c56600881e03a5fb27e3ce5f48a
dianetam523 <diane.tam523@gmail.com>
2016-04-03 19:29:45 -0400

    Hi-def terrain, added fog

--- src/renderEngine/MasterRenderer.java
+++ src/renderEngine/MasterRenderer.java
@@ -21,6 +21,12 @@ public class MasterRenderer {
 	private static final float FOV = 70; //field of view
 	private static final float NEAR_PLANE = 0.1f; 
 	private static final float FAR_PLANE = 1000; // how far you can see
+	
+	private static final float RED = 0.63f;
+	private static final float GREEN = 0.42f;
+	private static final float BLUE = 0.38f;
+
+	
 	private Matrix4f projectionMatrix;
 	
 	private StaticShader shader = new StaticShader();
@@ -44,6 +50,7 @@ public class MasterRenderer {
 	public void render(Light sun, Camera camera){
 		prepare();
 		shader.start();
+		shader.loadSkyColor(RED, GREEN, BLUE);
 		shader.loadLight(sun);
 		shader.loadViewMatrix(camera);
 		renderer.render(entities);
@@ -82,7 +89,7 @@ public class MasterRenderer {
 	public void prepare(){
 		GL11.glEnable(GL11.GL_DEPTH_TEST);
 		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT|GL11.GL_DEPTH_BUFFER_BIT);
-		GL11.glClearColor(0.56f, 0.76f, 0.83f, 1);
+		GL11.glClearColor(RED, GREEN, BLUE, 1);
 	}
 	
 	
